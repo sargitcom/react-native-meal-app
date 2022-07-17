c12bc3774e5f5338edc3c91a155a2032acc5d363
Kamil Pietrzkiewicz <k.pietrzkiewicz@yourcx.io>
2022-07-17 21:37:27 +0200

    Main - add drawer

--- App.js
+++ App.js
@@ -2,10 +2,32 @@ import CategoriesScreen from "./screens/CategoriesScreen";
 import {StatusBar, Text, Button} from "react-native";
 import {NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailsScreen from "./screens/MealDetailsScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+    return <Drawer.Navigator
+        screenOptions={{
+            headerStyle : {
+                backgroundColor: '#351401'
+            },
+            headerTintColor: 'white',
+            sceneContainerStyle : {
+                backgroundColor: '#3f2f25'
+            },
+            drawerContentStyle: { backgroundColor: '#351401' },
+            drawerActiveBackgroundColor: '#e4aab1',
+        }}
+    >
+        <Drawer.Screen name={"Categories"} component={CategoriesScreen} />
+        <Drawer.Screen name={"Favorites"} component={FavoritesScreen} />
+    </Drawer.Navigator>
+}
 
 export default function App() {
   return (
@@ -23,9 +45,9 @@ export default function App() {
                     }
                 }}
             >
-                <Stack.Screen name={"Meals Categories"} component={CategoriesScreen} options={{
-                    title: "All Categories",
-
+                <Stack.Screen name={"Meals Categories"} component={DrawerNavigator} options={{
+                    // title: "All Categories",
+                    headerShown: false,
                 }} />
                 <Stack.Screen
                     name={"Meals Overview Categories"}
---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,12 @@
+import { FlatList } from "react-native";
+import { CATEGORIES } from "../data/data";
+import CategoryGridTile from "../components/CategoryGridTile";
+import {} from '@react-navigation/native'
+
+const FavoritesScreen = ({ navigation }) => {
+    return <>
+
+    </>
+}
+
+export default FavoritesScreen;
